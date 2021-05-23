9028f07e5797f79790921dd85d0f15f7855a7b69
Mandy B <mandyblott@gmail.com>
2021-05-23 07:20:28 -0500

    Completed exercises

--- graph.js
+++ graph.js
@@ -11,27 +11,74 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray){
+      this.nodes.add(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (let node of this.nodes){
+      node.adjacent.delete(vertex)
+    }
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    let nodesVisited = []
+    let nodesToVisit = [start];
+    let nodesAddedToVisit = new Set([start]);
+
+    while(nodesToVisit.length){
+      let current = nodesToVisit.pop();
+      nodesVisited.push(current.value);
+      for (let neighbor of current.adjacent){
+        if(!nodesAddedToVisit.has(neighbor)){
+          nodesToVisit.push(neighbor);
+          nodesAddedToVisit.add(neighbor);
+        }
+      }
+    }
+    return nodesVisited;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
-}
+  breadthFirstSearch(start) {
+    let nodesVisited = []
+    let nodesToVisit = [start];
+    let nodesAddedToVisit = new Set([start]);
 
-console.log("hello")
+    while(nodesToVisit.length){
+      let current = nodesToVisit.shift();
+      nodesVisited.push(current.value);
+      for (let neighbor of current.adjacent){
+        if(!nodesAddedToVisit.has(neighbor)){
+          nodesToVisit.push(neighbor);
+          nodesAddedToVisit.add(neighbor);
+        }
+      }
+    }
+    return nodesVisited;
+  }
+}
 
 module.exports = {Graph, Node}
